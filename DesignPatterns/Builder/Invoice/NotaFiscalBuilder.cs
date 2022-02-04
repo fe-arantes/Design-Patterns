ae648c160686037368165c5bfe992ea7faf606f2
Felipe Arantes <58771056+fe-arantes@users.noreply.github.com>
2022-02-04 14:52:52 -0300

    Builder

---
+++ DesignPatterns/Builder/Invoice/NotaFiscalBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Builder.Invoice
+{
+    internal class NotaFiscalBuilder
+    {
+        private string _RazaoSocial;
+        private string _Cnpj;
+        private string _Note;
+        private DateTime _DataDeEmissao = DateTime.Now;
+        public decimal TotalValue => Items.Sum(x => x.Value);
+        public decimal TaxValue => Items.Sum(x => x.TaxValue);
+
+        private readonly IList<NotaFiscalItem> Items = new List<NotaFiscalItem>();
+
+        public NotaFiscalBuilder SetRazaoSocial(string razaoSocial)
+        {
+            _RazaoSocial = razaoSocial;
+            return this;
+        }
+
+        public NotaFiscalBuilder SetCnpj(string cnpj)
+        {
+            _Cnpj = cnpj;
+            return this;
+        }
+
+        public NotaFiscalBuilder SetDataDeEmissao(DateTime dataDeEmissao)
+        {
+            _DataDeEmissao = dataDeEmissao;
+            return this;
+        }
+
+        public NotaFiscalBuilder SetNote(string note)
+        {
+            _Note = note;
+            return this;
+        }
+
+        public NotaFiscalBuilder AddItem(NotaFiscalItem item)
+        {
+            Items.Add(item);
+            return this;
+        }
+
+        public NotaFiscal CreateNf()
+        {
+            var notaFiscal = new NotaFiscal(_RazaoSocial, _Cnpj, _DataDeEmissao, _Note);
+
+            notaFiscal.AddItems(Items);
+
+            return notaFiscal;
+        }
+    }
+}
---
+++ DesignPatterns/Builder/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using DesignPatterns.Builder.Invoice;
+
+var notaFiscalFactory = new NotaFiscalBuilder();
+notaFiscalFactory
+    .SetRazaoSocial("RazaoSocial")
+    .SetCnpj("Cnpj")
+    .SetNote("Note")
+    .AddItem(new NotaFiscalItem("item 1", 10, 3))
+    .AddItem(new NotaFiscalItem("item 2", 22, 7));
+
+var notaFiscal = notaFiscalFactory.CreateNf();
+
+Console.WriteLine($"TotalValue = { notaFiscal.TotalValue}");
+Console.WriteLine($"TaxValue = { notaFiscal.TaxValue}");
+Console.ReadKey();
